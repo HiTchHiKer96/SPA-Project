81e2012b089d7f95fef81a8ef2c015162397705d
HiTchHiKer96 <krzysiek.paszko.96@gmail.com>
2020-05-03 12:17:55 +0200

    Initial dist subtree commit

---
+++ precache-manifest.2875a33d5542f682212f979935f22776.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91531fb3b2b4582e48cc",
+    "url": "/SPA-Project/css/app.274415de.css"
+  },
+  {
+    "revision": "a3e02a3ec0b10fdb6caeafd3837e3fce",
+    "url": "/SPA-Project/index.html"
+  },
+  {
+    "revision": "91531fb3b2b4582e48cc",
+    "url": "/SPA-Project/js/app.edce0837.js"
+  },
+  {
+    "revision": "aa5c64c130ab72dc667e",
+    "url": "/SPA-Project/js/chunk-vendors.b6c25ce1.js"
+  },
+  {
+    "revision": "a5bc4f29997a752c49dd3d3f3af5b2fb",
+    "url": "/SPA-Project/main.css"
+  },
+  {
+    "revision": "614a88ca5f1fd48f1a034d0977fc5eb1",
+    "url": "/SPA-Project/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/SPA-Project/robots.txt"
+  }
+]);
